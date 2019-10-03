fd1d5ae966f5727dd409ff1a1e4b756748753e31
Emmanuel <meric.emmanuel@gmail.com>
2019-10-03 15:44:49 +0200

    added extra files

---
+++ BulbasaurOrPikachu/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BulbasaurOrPikachu
+//
+//  Created by epita on 03/10/2019.
+//  Copyright © 2019 Epita. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var pokemons: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
